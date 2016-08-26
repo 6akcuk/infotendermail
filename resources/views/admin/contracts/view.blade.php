11de3fd9fff63a5b8c8480d1e78118c654062eb8
Denis Sirashev <6akcuk@gmail.com>
2016-08-26 17:36:02 +0000

    Corrections for view contracts.

--- resources/views/admin/contracts/view.blade.php
+++ resources/views/admin/contracts/view.blade.php
@@ -1,40 +1,41 @@
-@extends('layouts.main')
+@extends('layouts.admin-ibox')
 
-@section('content')
-    <div class="ibox">
-        <div class="ibox-title">
-            <h3>{{ trans_choice('main.contracts', sizeof($list)) }}</h3>
-        </div>
-        <div class="ibox-content">
-            <div class="table-responsive">
-                <table id="contracts-table" class="table table-striped table-bordered table-hover">
-                    <thead>
-                    <tr>
-                        <th>Название</th>
-                        <th>Организация</th>
-                        <th>Адрес</th>
-                        <th>ИНН</th>
-                        <th>Цена</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach ($list as $contract)
-                    <tr>
-                        <td>
-                            <a href="{{ !stristr($contract->link, 'zakupki.gov.ru') ? 'http://zakupki.gov.ru'. $contract->link : $contract->link }}">
-                                {{ $contract->name }}
-                            </a>
-                        </td>
-                        <td>{{ $contract->organization->name }}</td>
-                        <td>{{ $contract->organization->address }}</td>
-                        <td>{{ $contract->organization->inn }}</td>
-                        <td style="width: 180px">{{ number_format($contract->price, 2, ',', ' ') }} руб.</td>
-                    </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
+@section('header', 'Контракты')
+
+@section('breadcrumbs')
+    <li>Контракты</li>
+@endsection
+
+@section('ibox-title', trans_choice('main.contracts', sizeof($list)))
+
+@section('ibox-content')
+    <div class="table-responsive">
+        <table id="contracts-table" class="table table-striped table-bordered table-hover">
+            <thead>
+            <tr>
+                <th>Название</th>
+                <th>Организация</th>
+                <th>Адрес</th>
+                <th>ИНН</th>
+                <th>Цена</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach ($list as $contract)
+            <tr>
+                <td>
+                    <a href="{{ !stristr($contract->link, 'zakupki.gov.ru') ? 'http://zakupki.gov.ru'. $contract->link : $contract->link }}">
+                        {{ $contract->name }}
+                    </a>
+                </td>
+                <td>{{ $contract->organization->name }}</td>
+                <td>{{ $contract->organization->address }}</td>
+                <td>{{ $contract->organization->inn }}</td>
+                <td style="width: 180px">{{ number_format($contract->price, 2, ',', ' ') }} руб.</td>
+            </tr>
+            @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
 
@@ -47,8 +48,8 @@
 
     <script>
         $(document).ready(function() {
-            $('#contracts-table').dataTable({
-            });
+            //$('#contracts-table').dataTable({
+            //});
         });
     </script>
 @endsection
